1031a730f2702d36cb4c04cf2b508fbe2ee49c44
Martin Flodin <martin.flodin@klarna.com>
2017-04-10 19:15:59 +0200

    add view for ski resort

--- static/app/controllers/view_skiresort_controller.js
+++ static/app/controllers/view_skiresort_controller.js
@@ -14,6 +14,25 @@
         $scope.ski_resort = ski_resort;
       }
     });
+
+    $scope.calculateAverage = function(ratings){
+      var sum = 0;
+      if ("undefined" === typeof ratings || ratings.length == 0) {
+        console.log("variable is undefined, or no ratings has been added");
+        return new Array(1);
+      }
+      console.log("ratings: " + ratings);
+      for(var i = 0; i < ratings.length; i++){
+        console.log("rating: " + ratings[i].score);
+        sum += parseInt(ratings[i].score, 10); //don't forget to add the base
+      }
+      var avg = sum/ratings.length;
+      return new Array(avg);
+    };
+
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    }
   }
 
   // 2. create the controller and give it $scope.
